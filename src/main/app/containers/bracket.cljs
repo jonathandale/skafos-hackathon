dbe5b1814c217f454089a6a7c3e67099c652dab9
Jon Dale <jon@metismachine.com>
2019-03-02 18:04:25 -0500

    even more code

--- src/main/app/containers/bracket.cljs
+++ src/main/app/containers/bracket.cljs
@@ -5,40 +5,77 @@
             [app.events :as events]
             [app.utils :refer [log]]))
 
+
+(def base-team-classes
+  ["rounded-sm" "text-sm" "px-4" "py-2" "my-1"])
+
+(def active-team-classes
+  (concat
+    base-team-classes
+    ["hover:bg-grey" "bg-grey-light" "cursor-pointer"]))
+
+(def inactive-team-classes
+  (concat
+    base-team-classes
+    ["bg-grey-lightest" "text-grey"]))
+
+(def pending-team-classes
+  (concat
+    base-team-classes
+    ["bg-grey-lighter" "text-grey-lighter"]))
+
 (defn- team-item [team matchup-info]
   (fn []
-    [:li.bg-grey-light.m-1.px-3.py-2.cursor-pointer.rounded-sm.text-sm
+    (log team)
+    [:li
      {:on-click #(dispatch [::events/select-team matchup-info])
-      :class (when (true? (:selected team))
-                   "text-grey")}
+      :class (if (true? (:selected team))
+               inactive-team-classes
+               active-team-classes)}
      (:name team)]))
 
-(defn- matchup [{:keys [teams round-index group-index]
-                 :as matchup-info}]
+(defn- empty-matchup []
+  [:<>
+   [:li {:class pending-team-classes} "."]
+   [:li {:class pending-team-classes} "."]])
+
+(defn- matchup
+  [{:keys [teams round-index group-index group-height]
+    :as matchup-info}]
   (fn []
-    [:ul.list-reset
-     (map-indexed
-       (fn [team-index team]
-         ^{:key team-index}
-          [team-item team (assoc matchup-info
-                                 :team-index team-index)])
-       teams)]))
+    [:div.flex.flex-col.justify-center
+     {:style {:height (str group-height "%")}}
+     [:ul.list-reset
+      (if (= :empty teams)
+        [empty-matchup]
+        (map-indexed
+          (fn [team-index team]
+            ^{:key team-index}
+             [team-item team (assoc matchup-info
+                                    :team-index team-index)])
+          teams))]]))
 
 (defn round [round-index]
   (let [round-teams (subscribe [::subs/bracket round-index])]
     (fn []
-      [:ul.list-reset.flex.flex-col.flex-grow.justify-between
-       (map-indexed
-         (fn [group-index teams]
-           ^{:key (str group-index round-index teams)}
-            [matchup {:teams teams
-                      :round-index round-index
-                      :group-index group-index}])
-         @round-teams)])))
+      (let [group-height (/ 100 (count @round-teams))]
+        [:ul.list-reset.mx-3
+         {:class "w-1/4"}
+         (map-indexed
+           (fn [group-index teams]
+             (log "teams" teams)
+             ^{:key (str group-index round-index teams)}
+              [matchup {:teams (if (empty? teams) :empty teams)
+                        :round-index round-index
+                        :group-index group-index
+                        :group-height group-height}])
+           @round-teams)]))))
 
 (defn render []
-  [:div.flex.h-screen.p-8
-   [round 0]
-   [round 1]
-   [round 2]
-   [round 3]])
+  [:div.flex
+   [:div.flex.h-screen.relative.p-8
+    {:class "w-1/2"}
+    [round 0]
+    [round 1]
+    [round 2]
+    [round 3]]])
--- src/main/app/events.cljs
+++ src/main/app/events.cljs
@@ -36,11 +36,9 @@
 
 
 ;; Bracket events
-
 (defn- calc-next-position [round-index team-index group-index next-round]
-  (prn (str "team index: " team-index " group index: " group-index " next round: " (count next-round) "current round: " current-round))
-  (prn (str "math before floor " (/ (* 2 group-index) (count next-round))))
-  (.floor js/Math (/ (* (- 2 round-index) group-index) (count next-round))))
+  (.floor js/Math (/ (* (- 2 round-index) group-index)
+                     (count next-round))))
 
 (re-frame/reg-event-db
   ::select-team
@@ -51,7 +49,6 @@
     (let [next-round (get-in db [:bracket (inc round-index)])
           next-group (calc-next-position round-index team-index group-index next-round)
           team (nth teams team-index)]
-      (log "Update " team " into" next-group)
       (-> db
         (update-in [:bracket round-index group-index team-index]
           (fn [team]
